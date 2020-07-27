8b576ffafaaf5261b39115e97dc4c4a1edbdd6b2
张峰 <zf@zillinx.com>
2020-07-27 15:03:58 +0800

    webapck-dev-server 作为热部署服务器

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -1,8 +1,18 @@
 const webpackBase = require('./webpack.base');
+const path = require('path');
 
 const devConfig = {
   devtool: 'source-map',
   mode: 'development',
-  // mode: 'production'
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    compress: true,
+    host: '0.0.0.0',
+    port: 9000,
+    hot: true,
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+  },
 };
 module.exports = Object.assign({}, webpackBase, devConfig);
